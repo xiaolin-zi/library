2c8c5d174732c91403ba4746620841861c56f69b
xiaolin <1849569695@qq.com>
2022-10-10 22:40:09 +0800

    修改完善登录2022-10-10

--- src/main/java/com/lxg/controller/KaptchaController.java
+++ src/main/java/com/lxg/controller/KaptchaController.java
@@ -1,6 +1,7 @@
 package com.lxg.controller;
 
 import com.google.code.kaptcha.impl.DefaultKaptcha;
+
 import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
